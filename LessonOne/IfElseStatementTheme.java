95574e0a1f15340e46f1deb244cbf6925e51a6d5
«Ivan <3923499@mail.ru>
2023-10-25 07:51:07 +0300

    Fixed task 3

--- LessonOne/IfElseStatementTheme.java
+++ LessonOne/IfElseStatementTheme.java
@@ -40,17 +40,50 @@ public class IfElseStatementTheme {
         }
 
         System.out.println("\n3.Проверка числа.\n");
-        int num3 = 0;
-        if (num3 % 2 == 0 && num3 > 0) {
-            System.out.println("Число чётное  и положительное");
-        } else if (num3 % 2 == 1 && num3 > 0) {
-            System.out.println("Число нечётное и положительное");
-        } else if (num3 % 2 == 0 && num3 < 0) {
-            System.out.println("Число чётное и отрицательное");
-        } else if ( - (num3 % 2) == 1 && num3 < 0) {
-            System.out.println("Число нечётное и отрицательное");
+        int num3 = 1;
+        if (num3 != 0 ) {
+            if (num3 % 2 == 0 && num3 > 0) {
+                System.out.println("Число " + num3 + " является чётным  и положительным.");
+            } else if (num3 % 2 == 1 && num3 > 0) {
+                System.out.println("Число " + num3 + " является нечётным и положительным.");
+            } else if(num3 % 2 == 0 && num3 < 0) {
+                System.out.println("Число " + num3 + " является чётным и отрицательным.");
+            } else if( - (num3 % 2) == 1 && num3 < 0) {
+                System.out.println("Число " + num3 + " является нечётным и отрицательным.");
+            }
         } else {
-            System.out.println("Число равно нулю");
+            System.out.println("Число " + num3 + " равно нулю");
         }
+        /*
+        if (num3 % 2 == 0 && num3 > 0) {
+            System.out.println("Число " + num3 + " чётное  и положительное");
+        } else if (num3 % 2 == 1 && num3 > 0) {
+            System.out.println("Число " + num3 + " нечётное и положительное");
+        } else if (num3 % 2 == 0 && num3 < 0) {
+            System.out.println("Число " + num3 + " чётное и отрицательное");
+        } else if ( - (num3 % 2) == 1 && num3 < 0) {
+            System.out.println("Число " + num3 + " нечётное и отрицательное");
+        } else {
+            System.out.println("Число " + num3 + " равно нулю");
+        }*/
+/*
+        System.out.println("\nПоиск одинаковых цифр в числах.\n");
+        int num4 = 123;
+        int num5 = 126;
+        int hundreds4 = num4 / 100;
+        int tens4 = (num4 / 10) % 10;
+        int ones4 = num4 % 10;
+        int hundreds5 = num5 / 100;
+        int tens5 = (num5 / 10) % 10;
+        int ones5 = num5 % 10;
+        if (hundreds4 != hundreds5 && tens4 != tens5 && ones4 != ones5) {
+            System.out.println("В этих числах нет равных цифр, стоящих в одинаковых разрядах");
+        } else if (hundreds4 == hundreds5) {
+            System.out.println("Числа " + num4 + " и " + num5 + " имеют равные цифры в третьем " +
+                    "разряде");
+        } else if (tens4 == tens5) {
+            System.out.println("Числа " + num4 + " и " + num5 + " имеют равные цифры во втором " +
+                    "разряде");
+        }*/
     }
 }
