8fa0e10053edec58e6d70114dc3ed51ace8b1c2b
«Ivan <3923499@mail.ru>
2023-11-29 08:24:53 +0300

    Task 8 Done

--- LessonOne/CyclesTheme.java
+++ LessonOne/CyclesTheme.java
@@ -69,22 +69,88 @@ public class CyclesTheme {
         //     }
         // }
 
-        System.out.println("\n5.Проверка количества двоек числа на четность/нечетность");
-        int num5 = 3242592;
-        int count1 = 0;
-        while(num5 % 2 == 0) {
-            count1 += 1;
-            num5 /= 2;
+        // System.out.println("\n5.Проверка количества двоек числа на четность/нечетность");
+        // int num5 = 3242592;
+        // int count1 = 0;
+        // while(num5 % 2 == 0) {
+        //     count1 += 1;
+        //     num5 /= 2;
+        // }
+        // int num6 = num5 * (int) Math.pow(2,count1);
+        // if(count1 % 2 == 0) {
+        //     System.out.println("В " + num6 + " чётное количество двоек - " + count1);
+        // } else {
+        //     System.out.println("В " + num6 + " нечётное количество двоек - " + count1);
+        // }
+
+        // System.out.println("\n6.Отображение геометрических фигур");
+        // int count2 = 0;
+        // for(int i = 1; i < 51; i++) {
+        //     if(count2 % 10 == 0) {
+        //         System.out.println();
+        //     }
+        //     System.out.print("*");
+        //     count2++;
+        // }
+        // System.out.println();
+        // System.out.println();
+
+
+        // int row = 5;
+        // int symbol = 5;
+        // int count3 = 0;
+        // while(row >= count3) {
+        //     while(symbol > count3) {
+        //         System.out.print("#");
+        //         symbol--;
+        //     }
+        //     System.out.println();
+        //     row--;
+        //     symbol = row;
+        // }
+
+        // int count4 = 2;
+        // int index = 2;
+        // int column = 9;
+        // do {
+        //     System.out.print('$');
+        //     if (index == count4){
+        //             if (count4 / 4 == 0) {
+        //                 count4++;
+        //             } else {
+        //                 count4--;
+        //             }
+        //             System.out.println();
+        //             index = 1;
+        //     }
+        //     index++;
+        //     column--;
+        // } while (column != 0);
+
+        // System.out.println("\n7.Отображение ASCII-символов");
+        // System.out.printf("%-11s%-11s%15s", "DECIMAL", "CHARACTER", "DESCRIPTION");
+        // for(char i = 39; i < 48; i+=2) {
+        //     System.out.printf("\n" + "  " + (int) i + "%12s%27s%n",i,Character.getName(i));
+        // }
+        // for(char i = 98; i < 123; i+=2) {
+        //     System.out.printf("\n" + "  " +  (int) i +  "%12s", i);
+        //     System.out.printf("%27s %n",Character.getName(i));
+        // }
+
+        System.out.println("\n8.Проверка, является ли число палиндромом");
+        int num7 = 1234321;
+        int temp = num7;
+        int reverseNum = 0;
+        int digit = 0;
+        while(num7 != 0) {
+            digit = num7 % 10;
+            reverseNum = reverseNum * 10 + digit;
+            num7 /= 10;
         }
-        int num6 = num5 * (int) Math.pow(2,count1);
-        if(count1 % 2 == 0) {
-            System.out.println("В " + num6 + " чётное количество двоек - " + count1);
+        if(reverseNum == temp) {
+            System.out.println("Число " + temp + " является палиндромом");
         } else {
-            System.out.println("В " + num6 + " нечётное количество двоек - " + count1);
+            System.out.println("Число " + temp + " не является палиндромом");
         }
-
-        System.out.println("\n6.Отображение геометрических фигур");
-        
-
     }
 }
