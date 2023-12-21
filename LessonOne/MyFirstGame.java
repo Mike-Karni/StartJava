4f036895fff425c004f12fc482393db39f02ca93
«Ivan <3923499@mail.ru>
2023-12-21 18:40:33 +0300

    Fixed Game

--- LessonOne/MyFirstGame.java
+++ LessonOne/MyFirstGame.java
@@ -1,15 +1,21 @@
 public class MyFirstGame {
     public static void main(String[] args) {
         int secretNumber = 80;
-        int guessNumber = 70;
+        int guessNumber = 100;
         if (secretNumber == guessNumber) {
             System.out.println("Вы победили!!!");
         } else {
+            while(secretNumber != guessNumber) {
                 if (guessNumber < secretNumber) {
                     System.out.println("Число " + guessNumber + " меньше загаданного компьютером.");
+                    guessNumber = (guessNumber + secretNumber) / 2 + 1;
                 } else {
                     System.out.println("Число " + guessNumber + " больше загаданного компьютером.");
+                    guessNumber = (guessNumber + secretNumber) / 2 - 1;
+                    guessNumber--;
                 }
+            }
+            System.out.println("Вы победили!!!");
         }
     }
 }
