2b4662292885e85c90c6dad4fdc3c253f544a331
«Ivan <3923499@mail.ru>
2023-12-21 07:58:44 +0300

    Changed variable

--- LessonOne/Calculator.java
+++ LessonOne/Calculator.java
@@ -2,38 +2,30 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("Программа 'Калькулятор'.");
         int a = 5;
-        int b = 2;
+        int b = 4;
         int result = 1;
-        char sign = '+';
-        int copyB = b;
+        char sign = '%';
         if (sign == '+') {
             result = a + b;
-            System.out.println(a + " " + sign + " " + b + " = " + result);
         } else if (sign == '-') {
             result = a - b;
-            System.out.println(a + " " + sign + " " + b + " = " + result);
         } else if (sign == '*') {
             result = a * b;
-            System.out.println(a + " " + sign + " " + b + " = " + result);
         } else if (sign == '/') {
             result = a / b;
-            System.out.println(a + " " + sign + " " + b + " = " + result);
         } else if (sign == '^') {
-            for (int i = copyB; copyB > 0; copyB--) {
+            int count = 0;
+            for (int i = b; b > 0; b--) {
                 result *= a;
+                count++;
             }
-            System.out.println(a + " " + sign + " " + b + " = " + result);
+            b = count;
         } else if (sign == '%') {
             result = a % b;
-            System.out.println(a + " " + sign + " " + b + " = " + result);
         } else {
             System.out.println("Вы ввели неправильный знак операции, исправьте ошибку и " + 
                     "перезапустите программу.");
         }
-
-
-
-
-
+        System.out.println(a + " " + sign + " " + b + " = " + result);
     }
 }
