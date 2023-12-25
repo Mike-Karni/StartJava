f493be670c0dc0200921c14f00a43b343d0e9522
«Ivan <3923499@mail.ru>
2023-12-25 20:52:07 +0300

    Person class Done

---
+++ LessonTwo/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String sex = "male";
+    String name = "Trofim";
+    float height = 1.89f;
+    float weight = 99.9f;
+    int age = 29;
+
+    void walk() {
+        System.out.println("Трофим быстро ходит!");
+    }
+
+    void sit() {
+        System.out.println("Трофим сидит.");
+    }
+
+    void run() {
+        System.out.println("Трофим быстро бежит.");
+    }
+
+    void speak() {
+        System.out.println("Трофим говорит спокойно и размеренно.");
+    }
+
+    void learnJava() {
+        System.out.println("Трофим усердно учит Java.");
+    }
+}
