6febaa95c0f769546d349512d637514e121b22f3
«Ivan <3923499@mail.ru>
2023-12-26 18:43:20 +0300

    Class works

---
+++ LessonTwo/Wolf.java
@@ -0,0 +1,27 @@
+public class Wolf {
+    String sex = "male";
+    String nickName = "Barbos";
+    int weight = 50;
+    int age = 10;
+    String color = "grey";
+
+    void walk() {
+        System.out.println("Волк идёт.");
+    }
+
+    void sit() {
+        System.out.println("Волк сидит.");
+    }
+
+    void run() {
+        System.out.println("Волк бежит.");
+    }
+
+    void howl() {
+        System.out.println("Волк воет.");
+    }
+
+    void hunt() {
+        System.out.println("Волк охотится.");
+    }
+}
