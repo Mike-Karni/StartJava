01a6b87baf1d467b15a8eb4c363bef7a7aadb7cc
«Ivan <3923499@mail.ru>
2023-12-27 18:47:02 +0300

    Output done

--- LessonTwo/WolfTest.java
+++ LessonTwo/WolfTest.java
@@ -2,17 +2,16 @@ public class WolfTest {
 
     public static void main(String[] args) {
         Wolf wolfOne = new Wolf();
-        wolfOne.sex = "female";
-        wolfOne.nickName = "Pampuha";
-        wolfOne.weight = 60;
-        wolfOne.age = 7;
-        wolfOne.color = "white";
-        System.out.println(wolfOne.sex);
-        System.out.println(wolfOne.nickName);
-        System.out.println(wolfOne.weight);
-        System.out.println(wolfOne.age);
-        System.out.println(wolfOne.color);
-
+        wolfOne.setSex("female");
+        wolfOne.setNickName("Dusya");
+        wolfOne.setWeight(30);
+        wolfOne.setAge(10);
+        wolfOne.setColor("White");
+        System.out.println(wolfOne.getSex());
+        System.out.println(wolfOne.getNickName());
+        System.out.println(wolfOne.getWeight());
+        System.out.println(wolfOne.getAge());
+        System.out.println(wolfOne.getColor());
         wolfOne.walk();
         wolfOne.sit();
         wolfOne.run();
