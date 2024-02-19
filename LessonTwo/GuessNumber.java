8dffae0def50836762e23cb24e60563386ab1185
«Ivan <3923499@mail.ru>
2024-02-19 18:41:32 +0300

    fixed2

--- LessonTwo/GuessNumber.java
+++ LessonTwo/GuessNumber.java
@@ -4,7 +4,6 @@ public class GuessNumber {
     private int guessNumber;
     private Player player1;
     private Player player2;
-    private String inputAnswer;
 
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
@@ -15,7 +14,7 @@ public class GuessNumber {
         Scanner scanner = new Scanner(System.in);
         int guessNumber = 0;
         int secretNumber = (int) (Math.random() * 101);
-        while(secretNumber != guessNumber) {
+        while(true) {
             System.out.print("Попытка угадать число игрока " + player1 + " : ");
             guessNumber = scanner.nextInt();
             if (guessNumber < secretNumber) {
@@ -24,7 +23,7 @@ public class GuessNumber {
                 System.out.println("Число " + guessNumber + " больше загаданного компьютером.");
             } else {
                 System.out.println("Победил игрок " + player1);
-                continue;
+                break;
             }
             System.out.print("Попытка угадать игрока " + player2 + " : ");
             guessNumber = scanner.nextInt();
@@ -34,7 +33,7 @@ public class GuessNumber {
                 System.out.println("Число " + guessNumber + " больше загаданного компьютером.");
             } else  {
                 System.out.println("Победил игрок " + player2);
-                continue;
+                break;
             }
         }
     }
