7f3e1e7478e2aaa1089e27d3111e19cc289d59b8
«Ivan <3923499@mail.ru>
2024-07-04 23:10:12 +0300

    Suffering with calculator )))

--- src/com/startjava/lesson_2_3/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3/calculator/Calculator.java
@@ -4,8 +4,12 @@ import java.util.Scanner;
 
 public class Calculator {
     private int num1;
-    private char sign;
+    //private char sign;
+
+    private String sign;
     private int num2;
+
+    private String line;
 
     public int getNum1() {
         return num1;
@@ -15,9 +19,9 @@ public class Calculator {
         this.num1 = num1;
     }
 
-    public void setSign(char sign) {
-        this.sign = sign;
-    }
+    //public void setSign(char sign) { this.sign = sign; }
+
+    public void setSign(String sign) { this.sign = sign; }
 
     public int getNum2() {
         return num2;
@@ -27,25 +31,58 @@ public class Calculator {
         this.num2 = num2;
     }
 
-    public double calculate() {
+    public void setMath(String line) { this.line = line; }
+
+    public String getMath() { return line; }
+
+
+//    public double calculate() {
+//        double result = 1;
+//        switch(sign) {
+//            case '+' :
+//                return num1 + num2;
+//            case '-' :
+//                return num1 - num2;
+//            case '*' :
+//                return num1 * num2;
+//            case '/' :
+//                return num1 / num2;
+//            case '%' :
+//                return num1 % num2;
+//            case '^' :
+//                for (int i = 0; i < num2; i++) {
+//                    result *= num1;
+//                }
+//                return result;
+//            default :
+//                System.out.println("Неправильный знак математической операции.");
+//                break;
+//        }
+//        return result;
+//    }
+
+    public double calculate(String line) {
+//        Scanner scanner = new Scanner(System.in);
+//        line = scanner.nextLine();
+        String[] symbols = line.split(" ", 0);
+        double firstOperand = Double.parseDouble(symbols[0]);
+        double secondOperand = Double.parseDouble(symbols[2]);
+        sign = symbols[1];
         double result = 1;
         switch(sign) {
-            case '+' : 
-                return num1 + num2;
-            case '-' :
-                return num1 - num2;
-            case '*' :
-                return num1 * num2;
-            case '/' :
-                return num1 / num2;
-            case '%' :
-                return num1 % num2;
-            case '^' :
-                for (int i = 0; i < num2; i++) {
-                    result *= num1;
-                }
-                return result;
-            default : 
+            case "+" :
+                return firstOperand + secondOperand;
+            case "-" :
+                return firstOperand - secondOperand;
+            case "*" :
+                return firstOperand * secondOperand;
+            case "/" :
+                return firstOperand / secondOperand;
+            case "%" :
+                return firstOperand % secondOperand;
+            case "^" :
+                return Math.pow(firstOperand,secondOperand);
+            default :
                 System.out.println("Неправильный знак математической операции.");
                 break;
         }
--- src/com/startjava/lesson_2_3/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3/calculator/CalculatorTest.java
@@ -1,5 +1,8 @@
 package com.startjava.lesson_2_3.calculator;
 
+
+import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class CalculatorTest {
@@ -10,17 +13,24 @@ public class CalculatorTest {
         String inputAnswer = scanner.nextLine().toLowerCase();
         while (!inputAnswer.equals("no")) {
             if (inputAnswer.equals("yes")) {
-                System.out.print("Введите первое число : ");
-                calculator.setNum1(scanner.nextInt());
-                System.out.print("Введите знак математической операции : ");
-                calculator.setSign(scanner.next().charAt(0));
-                System.out.print("Введите второе число : ");
-                calculator.setNum2(scanner.nextInt());
-                System.out.println("Результат вычисления = " + calculator.calculate());
+                System.out.println("Введите математическое выражение в формате a 'знак операции' b");
+                calculator.setMath(scanner.nextLine());
+                String[] symbols = calculator.getMath().split(" ", 0);
+                if (Double.isNaN(Double.parseDouble(symbols[0])) || Double.isNaN(Double.parseDouble(symbols[2]))) {
+                    System.out.println("Вы ввели не число");
+                }
+                for (int i = 0; i < symbols.length; i++) {
+                    if (i < 2) {
+                        System.out.print(symbols[i] + " ");
+                    } else {
+                        System.out.print(symbols[i] + " = ");
+                    }
+                }
+                System.out.printf("%.3f", calculator.calculate(calculator.getMath()));
             } else {
                 System.out.println("Введите корректный ответ - [yes / no]");
             }
-            System.out.println("Вы желаете продолжить вычисления?[yes/no]: ");
+            System.out.println("\nВы желаете продолжить вычисления?[yes/no]: ");
             inputAnswer = scanner.nextLine().toLowerCase();
         }
         System.out.println("Закончили работу.");
