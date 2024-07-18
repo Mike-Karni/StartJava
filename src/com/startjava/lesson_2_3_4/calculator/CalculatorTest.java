a80e86f94861e7bbaf76f296f418b8f03ab1604a
«Ivan <3923499@mail.ru>
2024-07-18 07:12:48 +0300

    Only one question

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -7,19 +7,16 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Желаете начать вычисления? [yes/no]");
-        String inputAnswer = scanner.nextLine().toLowerCase();
-        while (!inputAnswer.equals("no")) {
-            if (inputAnswer.equals("yes")) {
-                System.out.println("Введите математическое выражение в формате a 'знак операции' b");
-                double result = calculator.calculate();
-                DecimalFormat decimal = new DecimalFormat("#.###");
-                System.out.println(decimal.format(result));
-            } else {
-                System.out.println("Введите корректный ответ - [yes / no]");
-            }
+        while (true) {
+            System.out.println("Введите математическое выражение в формате a 'знак операции' b");
+            double result = calculator.calculate();
+            DecimalFormat decimal = new DecimalFormat("#.###");
+            System.out.println(decimal.format(result));
             System.out.println("Вы желаете продолжить вычисления?[yes/no]: ");
-            inputAnswer = scanner.nextLine().toLowerCase();
+            String inputAnswer = scanner.nextLine().toLowerCase();
+            if (inputAnswer.equals("no")) {
+                break;
+            }
         }
         System.out.println("Закончили работу.");
     }
